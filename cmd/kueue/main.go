d23966868a3394b484cd7d3a815acad61a46c2de
Khurram Baig <kbaig@redhat.com>
2025-09-02 15:55:56 +0530

    [KEP-2349] Implementation of Generic Job MultiKueue Adaptor
    This code implements KEP 2349.

--- cmd/kueue/main.go
+++ cmd/kueue/main.go
@@ -357,7 +357,7 @@ func setupControllers(ctx context.Context, mgr ctrl.Manager, cCache *schdcache.C
 				setupLog.Error(err, "Could not load generic adapter configurations")
 				os.Exit(1)
 			}
-			setupLog.Info("inside generic creation")
+			setupLog.Info("Creating generic adapters")
 
 			// Add generic adapters to the adapters map
 			for _, adapter := range genericConfigManager.GetAllAdapters() {
